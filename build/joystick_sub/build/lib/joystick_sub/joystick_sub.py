b8692b8d11a45fce0e19a06c096f4e6008ec5b4e
Simercher <Simercher@gmail.com>
2023-10-22 20:16:44 +0800

    mecanum control finished

--- build/joystick_sub/build/lib/joystick_sub/joystick_sub.py
+++ build/joystick_sub/build/lib/joystick_sub/joystick_sub.py
@@ -1,9 +1,31 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Int8, Int32
+from std_msgs.msg import Int8, Float32MultiArray
 from sensor_msgs.msg import Joy
+from math import cos, sin
 
+F310_CODE_MAP = {
+    'ABS_X': 0,
+    'ABS_Y': 1,
+    'ABS_Z': 2,
+    'ABS_RX': 3,
+    'ABS_RY': 4,
+    'ABS_RZ': 5,
+    'ABS_HAT0X': 6,
+    'ABS_HAT0Y': 7,
+    'BTN_SOUTH': 0,
+    'BTN_EAST': 1,
+    'BTN_NORTH': 2,
+    'BTN_WEST': 3,
+    'BTN_TL': 4,
+    'BTN_TR': 5,
+    'BTN_SELECT': 6,
+    'BTN_START': 7,
+    # 'BTN_MODE': 8,
+    'BTN_THUMBL': 9,
+    'BTN_THUMBR':10
+}
 
 class Joystick_sub(Node):
 
@@ -13,38 +35,70 @@ class Joystick_sub(Node):
             Joy,
             'joy',
             self.listener_callback,
-            10)
+            1)
         self.subscription  # prevent unused variable warning
         
         # publisher
-        self.publisher_ = self.create_publisher(Int8, 'topic', 10)
-        timer_period = 0.01  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = Int8()
-        self.count = 0
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'topic', 1)
+        # timer_period = 0.01  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        # self.i = Int16()
+        # self.count = 0
+        self.control_matrix = Float32MultiArray(data = [0.0, 0.0, 0.0, 0.0, 0.0])
 
     def listener_callback(self, msg):
-        for i in msg.buttons:
-            if i:
-                self.get_logger().info('"%s"' % i)
-        for i in msg.axes:
-            if i:
-                # if self.count % 3 == 1:
-                self.i.data = round(i*100)
-                break
-            else:
-                self.i.data = 0
-        self.count += 1
-        self.get_logger().info('"%f"' % self.i.data)
-        self.get_logger().info('Publishing: "%d"' % self.count)
+        # for i in msg.buttons:
+        #     if i:
+        #         self.get_logger().info('"%s"' % i)
+        # print("Buttons", msg.buttons)
+        print("Axes", msg.axes)
+        axes = msg.axes
+        # print(self.control_matrix)
+        self.control_matrix.data = output = self.calculate_mecanum(X = -axes[0], Y = axes[1], Rotate = -axes[3])
+        print(output)
+        self.publisher_.publish(self.control_matrix)
+        # for i in msg.axes:
+        #     if i:
+        #         # if self.count % 3 == 1:
+        #         index = msg.axes.index(i)
+        #         self.get_logger().info('"%d"' % index)
+        #         self.i.data = 255 if round(i*255) > 255 else round(i*255)
+        #         break
+        #     else:
+        #         # index = msg.axes.index(i)
+        #         # self.get_logger().info('"%d"' % index)
+        #         self.i.data = 0
+        # # self.count += 1
+        # self.get_logger().info('"%f"' % self.i.data)
+        # self.get_logger().info('Publishing: "%d"' % self.count)
 
-    def timer_callback(self):
-        # msg = String()
-        # msg.data = 'Hello World: %d' % self.i
-        # if self.i.data >0:
-        self.publisher_.publish(self.i)
-        # self.get_logger().info('Publishing: "%d"' % self.i.data)
-        # self.i += 1
+    # def timer_callback(self):
+    #     # msg = String()
+    #     # msg.data = 'Hello World: %d' % self.i
+    #     # if self.i.data >0:
+    #     self.publisher_.publish(self.i)
+    #     # self.get_logger().info('Publishing: "%d"' % self.i.data)
+    #     # self.i += 
+    
+    def calculate_mecanum(self, X=0, Y=0, Gyro=0, Rotate=0):
+        control_matrix_data = self.control_matrix.data
+        Y = Y*cos(Gyro) + X*sin(Gyro)
+        X = X*cos(Gyro) - Y*sin(Gyro)
+        deltaI = Y + X # calaulate FL RR
+        deltaJ = Y - X # calaulate RL FR
+        control_matrix_data[0] = deltaI + Rotate # calaulate FL
+        control_matrix_data[3] = deltaI - Rotate # calaulate RR
+        control_matrix_data[1] = deltaJ + Rotate # calaulate RL
+        control_matrix_data[2] = deltaJ - Rotate # calaulate FR
+        
+        for i in range(4):
+            if control_matrix_data[i] > 1:
+                control_matrix_data[i] /= control_matrix_data[i]
+            elif control_matrix_data[i] < -1:
+                control_matrix_data[i] /= -control_matrix_data[i]
+            control_matrix_data[i] = control_matrix_data[i] * 255.0
+        # print(control_matrix_data)
+        return control_matrix_data
 
 def main(args=None):
     rclpy.init(args=args)
--- src/joystick_sub/joystick_sub/pubsub/pubsub.ino
+++ src/joystick_sub/joystick_sub/pubsub/pubsub.ino
@@ -5,38 +5,86 @@
 
 #include <ros.h>
 #include <std_msgs/String.h>
-#include <std_msgs/Int16.h>
+#include <std_msgs/Float32MultiArray.h>
+
+#define LIN (int []) {22, 23, 24, 25} // FL RL
+#define RIN (int []) {26, 27, 28, 29} // FR RR
+#define PWM (int []) {12, 13, 10, 11} // FL RL FR RR
 
 ros::NodeHandle  nh;
-char str1[50] = "The number is "; 
-
-void messageCb( const std_msgs::Int16& msg){
-//  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  sprintf(str1, "%d", msg.data);
-  nh.loginfo(str1);
+//char str1[50] = "The number is "; 
+int index = 0;
+void messageCb( const std_msgs::Float32MultiArray& msg){
+  //set FL
+  if (msg.data[0] >= 0) {
+    digitalWrite(LIN[0], LOW);
+    digitalWrite(LIN[1], HIGH);
+    analogWrite(PWM[0], msg.data[0]);
+  } else {
+    digitalWrite(LIN[0], HIGH);
+    digitalWrite(LIN[1], LOW);
+    analogWrite(PWM[0], -msg.data[0]);
+  }
+  //set RL
+  if (msg.data[1] >= 0) {
+    digitalWrite(LIN[2], LOW);
+    digitalWrite(LIN[3], HIGH);
+    analogWrite(PWM[1], msg.data[1]);
+  } else {
+    digitalWrite(LIN[2], HIGH);
+    digitalWrite(LIN[3], LOW);
+    analogWrite(PWM[1], -msg.data[1]);
+  }
+  //set FR
+  if (msg.data[2] >= 0) {
+    digitalWrite(RIN[0], HIGH);
+    digitalWrite(RIN[1], LOW);
+    analogWrite(PWM[2], msg.data[2]);
+  } else {
+    digitalWrite(RIN[0], LOW);
+    digitalWrite(RIN[1], HIGH);
+    analogWrite(PWM[2], -msg.data[2]);
+  }
+  //set RR
+  if (msg.data[3] >= 0) {
+    digitalWrite(RIN[2], HIGH);
+    digitalWrite(RIN[3], LOW);
+    analogWrite(PWM[3], msg.data[3]);
+  } else {
+    digitalWrite(RIN[2], LOW);
+    digitalWrite(RIN[3], HIGH);
+    analogWrite(PWM[3], -msg.data[3]);
+  }
+//  sprintf(str1, "%d", msg.data);
+//  nh.loginfo(str1);
 }
 
-ros::Subscriber<std_msgs::Int16> sub("topic", messageCb );
-
-
-
-//std_msgs::String str_msg;
-//ros::Publisher chatter("chatter", &str_msg);
-
-//char hello[13] = "hello world!";
+ros::Subscriber<std_msgs::Float32MultiArray> sub("topic", messageCb );
 
 void setup()
 {
-//  pinMode(13, OUTPUT);
+  // set FL
+  pinMode(LIN[0], OUTPUT);
+  pinMode(LIN[1], OUTPUT);
+  pinMode(PWM[0], OUTPUT);
+  // set RL
+  pinMode(LIN[2], OUTPUT);
+  pinMode(LIN[3], OUTPUT);
+  pinMode(PWM[1], OUTPUT);
+  // set FR
+  pinMode(RIN[0], OUTPUT);
+  pinMode(RIN[1], OUTPUT);
+  pinMode(PWM[2], OUTPUT);
+  // set RR
+  pinMode(RIN[2], OUTPUT);
+  pinMode(RIN[3], OUTPUT);
+  pinMode(PWM[3], OUTPUT);
   nh.initNode();
-//  nh.advertise(chatter);
   nh.subscribe(sub);
 }
 
 void loop()
 {
-//  str_msg.data = hello;
-//  chatter.publish( &str_msg );
   nh.spinOnce();
   delay(10);
 }
